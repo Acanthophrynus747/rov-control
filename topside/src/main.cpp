de07a9ef56ba54feac75ddf48f77683dc5fac6ed
Acanthophrynus747 <jacksbuilds@gmail.com>
2025-10-25 20:39:55 -0700

    working on communication between ROV and control

--- topside/src/main.cpp
+++ topside/src/main.cpp
@@ -9,20 +9,24 @@ const int L_stick_y_pin = 35;
 const int R_stick_x_pin = 32;
 const int R_stick_y_pin = 33;
 
-const int motor1_fwd = 5; //maybe CHANGE LATER TOO
-const int motor1_rev = 18;
-const int motor2_fwd = 19; //MOTOR CONTROLLERS
-const int motor2_rev = 21;
-const int motor3_fwd = 22; //motor 1 left fwd, 2 right fwd, 3 up/dn
-const int motor3_rev = 23;
+// const int motor1_fwd = 5; //maybe CHANGE LATER TOO
+// const int motor1_rev = 18;
+// const int motor2_fwd = 19; //MOTOR CONTROLLERS
+// const int motor2_rev = 21;
+// const int motor3_fwd = 22; //motor 1 left fwd, 2 right fwd, 3 up/dn
+// const int motor3_rev = 23;
 
 const int enable_motors_pin = 25; //switch to enable motors/change state
 
-// int joy_x, joy_y, joy_up;
+// variables for control inputs to send to ROV
 int motor1_pwr, motor2_pwr, motor3_pwr;
+bool lights_on = true;
 
 int motor1_thrust(int joy_x, int joy_y);
 int motor2_thrust(int joy_x, int joy_y);
+int motor3_thrust(int joy_x, int joy_y);
+
+String controlMsg(int motor1_pwr, int motor2_pwr, int motor3_pwr, bool lights_on);
 
 void off(void);
 void run(void);
@@ -43,12 +47,12 @@ void setup(){
     pinMode(R_stick_x_pin, INPUT);
     pinMode(R_stick_y_pin, INPUT);
 
-    pinMode(motor1_fwd, OUTPUT);
-    pinMode(motor1_rev, OUTPUT);
-    pinMode(motor2_fwd, OUTPUT);
-    pinMode(motor2_rev, OUTPUT);
-    pinMode(motor3_fwd, OUTPUT);
-    pinMode(motor3_rev, OUTPUT);
+    // pinMode(motor1_fwd, OUTPUT);
+    // pinMode(motor1_rev, OUTPUT);
+    // pinMode(motor2_fwd, OUTPUT);
+    // pinMode(motor2_rev, OUTPUT);
+    // pinMode(motor3_fwd, OUTPUT);
+    // pinMode(motor3_rev, OUTPUT);
 }
 
 void loop(){
@@ -84,36 +88,16 @@ void run(){
     motor2_pwr = motor2_thrust(R_stick_x, R_stick_y);
     motor3_pwr = motor3_thrust(L_stick_x, L_stick_y);
 
-    // power to motors
-    if (motor1_pwr >= 0){
-        analogWrite(motor1_fwd, motor1_pwr);
-    }
-    else if (motor1_pwr < 0){
-        analogWrite(motor1_rev, abs(motor1_pwr));
-    }
+    String control_msg = controlMsg(motor1_pwr, motor2_pwr, motor3_pwr, lights_on);
 
-    if (motor2_pwr >= 0){
-        analogWrite(motor2_fwd, motor2_pwr);
-    }
-    else if (motor1_pwr < 0){
-        analogWrite(motor2_rev, abs(motor2_pwr));
-    }
-
-    if (motor3_pwr >= 0){
-        analogWrite(motor3_fwd, motor3_pwr);
-    }
-    else if (motor3_pwr < 0){
-        analogWrite(motor3_rev, abs(motor3_pwr));
-    }
-
-    if (digitalRead(enable_motors_pin) == LOW){
-        state = MOTORS_OFF;
-    }
+    
 }
 
 int motor1_thrust(int joy_x, int joy_y){
-    int x = map(joy_x, 0, 1023, -255, 255);
-    int y = map(joy_y, 0, 1023, -255, 255);
+    // int x = map(joy_x, 0, 1023, -255, 255);
+    // int y = map(joy_y, 0, 1023, -255, 255);
+    int x = map(joy_x, 0, 1023, 0, 510);
+    int y = map(joy_y, 0, 1023, 0, 510); //changed to 0-510 to avoid having to send a negative number over serial
 
     int motor_thrust = (x + y);
 
@@ -121,8 +105,8 @@ int motor1_thrust(int joy_x, int joy_y){
 }
 
 int motor2_thrust(int joy_x, int joy_y){
-    int x = map(joy_x, 0, 1023, -255, 255);
-    int y = map(joy_y, 0, 1023, -255, 255);
+    int x = map(joy_x, 0, 1023, 0, 510);
+    int y = map(joy_y, 0, 1023, 0, 510);
     
     int motor_thrust = ((-1 * joy_x) + joy_y);
 
@@ -130,9 +114,14 @@ int motor2_thrust(int joy_x, int joy_y){
 }
 
 int motor3_thrust(int joy_x, int joy_y){
-    int y = map(joy_x, 0, 1023, -255, 255);
+    int y = map(joy_y, 0, 1023, 0, 510);
 
     int motor_thrust = y;
 
     return motor_thrust;
+}
+
+String controlMsg(int motor1_pwr, int motor2_pwr, int motor3_pwr, bool lights_on){
+    String control_str = "A" + String(motor1_pwr) + "B" + String(motor2_pwr) + "C" + String(motor3_pwr) + "D" + String(lights_on);
+    return control_str;
 }
