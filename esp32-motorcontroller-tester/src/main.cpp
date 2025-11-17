5b731f72e0bf944f3a98e05cf5a72692b1fb02cf
Acanthophrynus747 <jacksbuilds@gmail.com>
2025-11-16 21:13:18 -0800

    finished testing motor controllers

--- esp32-motorcontroller-tester/src/main.cpp
+++ esp32-motorcontroller-tester/src/main.cpp
@@ -11,26 +11,9 @@ const int motor1_rev = 25;
 // const int motor3_fwd = 33;
 // const int motor3_rev = 32;
 
-const int pwm_freq = 10000; // Hz
-const int pwm_resolution = 8; // 8 bit, 0 to 255 value
-const int pwm_channel = 0;
-
-// ESP32PWM pwm;
-
-
 
 void setup(){
     Serial.begin(115200);
-
-    // pinMode(motor1_fwd, OUTPUT);
-    // pinMode(motor1_rev, OUTPUT);
-
-    // ESP32PWM::allocateTimer(0);
-    // ESP32PWM::allocateTimer(1);
-    // ESP32PWM::allocateTimer(2);
-    // ESP32PWM::allocateTimer(3);
-
-    // pwm.attachPin(motor1_fwd, pwm_freq, pwm_resolution);
     
     pinMode(motor1_fwd, OUTPUT);
     pinMode(motor1_rev, OUTPUT);
@@ -44,32 +27,27 @@ void loop(){
 
     Serial.println("fwd 255");
     analogWrite(motor1_fwd, 255);
-    // analogWrite(motor1_rev, 0);
-    // ledcWrite(motor1_fwd, 255);
-    // pwm.write(255);
     delay(5000);
 
     Serial.println("fwd 100");
     analogWrite(motor1_fwd, 100);
-    // analogWrite(motor1_rev, 0);
-    // ledcWrite(motor1_fwd, 100);
-    // pwm.write(100);
-    // delay(5000);
-
-    // Serial.println("stop");
-    // analogWrite(motor1_fwd, 0);
-    // analogWrite(motor1_rev, 0);
-    // ledcWrite(motor1_fwd, 0);
-    // pwm.write(0);
     delay(5000);
 
-    // Serial.println("rev 100");
-    // analogWrite(motor1_rev, 100);
-    // // ledcWrite(motor1_rev, 100);
-    // delay(2000);
+    Serial.println("stop");
+    analogWrite(motor1_fwd, 0);
+    analogWrite(motor1_rev, 0);
+    delay(5000);
 
-    // Serial.println("stop");
-    // analogWrite(motor1_rev, 0);
-    // // ledcWrite(motor1_rev, 0);
-    // delay(2000);
+    Serial.println("rev 100");
+    analogWrite(motor1_rev, 100);
+    delay(5000);
+
+    Serial.println("rev 255");
+    analogWrite(motor1_rev, 255);
+    delay(5000);
+
+    Serial.println("stop");
+    analogWrite(motor1_rev, 0);
+
+    delay(5000);
 }
